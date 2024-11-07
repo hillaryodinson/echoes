08db041c21f98627727ed721a8675ceb8ba767cd
Hillary Sylvester <insanedude.developer@gmail.com>
2024-11-07 23:05:01 +0100

    Prevent Dialog from closing on bg click

--- src/components/custom/generic/modal.tsx
+++ src/components/custom/generic/modal.tsx
@@ -1,41 +1,53 @@
+import { Button } from "@/components/ui/button";
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
 } from "@/components/ui/dialog";
 import { cn } from "@/lib/utils";
+import { X } from "lucide-react";
 import React, { SetStateAction } from "react";
 
 interface modalProps {
-  isOpen: boolean;
-  setIsOpen: React.Dispatch<SetStateAction<boolean>>;
-  title?: string;
-  description?: string;
-  children: React.ReactNode;
-  className?: string;
+	isOpen: boolean;
+	setIsOpen: React.Dispatch<SetStateAction<boolean>>;
+	title?: string;
+	description?: string;
+	children: React.ReactNode;
+	className?: string;
+	closeOnBgClick?: boolean;
 }
 function Modal({
-  isOpen,
-  setIsOpen,
-  title,
-  description,
-  children,
-  className,
+	isOpen,
+	setIsOpen,
+	title,
+	description,
+	children,
+	className,
+	closeOnBgClick = true,
 }: modalProps) {
-  return (
-    <Dialog onOpenChange={setIsOpen} open={isOpen}>
-      <DialogContent className={cn("overflow-y-auto", className)}>
-        <DialogHeader>
-          <DialogTitle>{title}</DialogTitle>
-          <DialogDescription>{description}</DialogDescription>
-          <div className="grid gap-4 py-4">{children}</div>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
-  );
+	return (
+		<Dialog onOpenChange={setIsOpen} open={isOpen} modal={true}>
+			<DialogContent
+				className={cn("overflow-y-auto", className)}
+				onInteractOutside={
+					closeOnBgClick
+						? () => setIsOpen(false)
+						: (e) => {
+								e.preventDefault();
+						  }
+				}>
+				<DialogHeader>
+					<DialogTitle>{title}</DialogTitle>
+					<DialogDescription>{description}</DialogDescription>
+				</DialogHeader>
+				<div className="grid gap-4 py-4">{children}</div>
+			</DialogContent>
+		</Dialog>
+	);
 }
 
 export default Modal;
